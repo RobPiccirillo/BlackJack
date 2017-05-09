da1817c7c8e60eac610b7ada3a9285c21d68ac6c
jlk22317 <joshk7998@gmail.com>
2017-05-09 19:44:10 -0400

    Update BlackJack.h

--- BlackJack.h
+++ BlackJack.h
@@ -1,1 +1,41 @@
+/*===============================================
+Program that simulates the BlackJack card game 
+Authors: Josh Kiss, Rob Piccirillo
+===============================================*/
 
+#include <iostream> //for cout
+#include <vector>   //for vector
+#include <string>
+#include <cstdlib>  //for rand
+using namespace std;
+
+#ifndef BLACKJACK_H_
+#define BLACKJACK_H_
+
+#define BLACKJACK 21
+
+class Player
+{
+public:
+Player();
+
+private:
+int m_hand;
+}
+
+class BlackJack: public Player
+{
+	public:
+	BlackJack(); //default constructor
+  
+  void hit();
+  void stand();
+  
+  private:
+  int m_val;
+  int m_playersHand;
+  int m_dealersHand;
+  
+};//blackjack
+
+#endif
