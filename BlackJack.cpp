b1bc89095d50f08faeae6ce5bf4b789488165a64
jlk22317 <joshk7998@gmail.com>
2017-05-10 12:48:18 -0400

    Update BlackJack.cpp

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -2,7 +2,29 @@
 
 BlackJack::BlackJack()
 {
+m_val;
+vector <int> m_playersHand;
+vector <int> m_dealersHand;
+  
+m_playersHandSum = 0;
+m_dealershandSum = 0;
+}
+
+BlackJack BlackJack::shuffleDeck()
+{
+  
+}
+int BlackJack::drawCard()
+{
   
 }
 
+void BlackJack::hit()
+{
+  
+}
 
+void BlackJack::stand()
+{
+  
+}
