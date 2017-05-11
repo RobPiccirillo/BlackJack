d73a521782a93a39547e500160f3e11fd937ab45
jlk22317 <joshk7998@gmail.com>
2017-05-11 12:06:14 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
+/*--------------------------------------
+main.cpp
+compiled with BlackJack.cpp
+BlackJack driver program
+Authors: Josh Kiss, Rob Piccirillo
+---------------------------------------*/
+#include "BlackJack.h"
 
+int main()
+{
+  cout<<"Welcome to BlackJack\n Ready to Play? (Y/N)"<<endl;
+  char start;
+  cin>>start;
+  
+  bool run = true;
+  
+  while(run)
+  {
+   BlackJack Game;
+    //everything else
+    
+  }
+ return 0; 
+}
